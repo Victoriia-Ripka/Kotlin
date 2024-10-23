65ac0a26d8c258541d4ad35685a349e8ea561612
Victoriia-Ripka <vikaripka@gmail.com>
2024-10-23 18:44:05 +0200

    lab3 fixed bugs

--- PW-3_TV-13_NowotkaViktoriia/app/src/main/java/com/example/pw3/MainActivity.kt
+++ PW-3_TV-13_NowotkaViktoriia/app/src/main/java/com/example/pw3/MainActivity.kt
@@ -5,11 +5,10 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.example.pw3.R
-import kotlin.math.pow
+//import kotlin.math.pow
+import com.example.pw3.round as round
 
 class MainActivity: AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -27,6 +26,49 @@ class MainActivity: AppCompatActivity() {
 
         val profit: TextView = findViewById(R.id.profit)
 
+        fun showResult(profitValue: Double) {
+            profit.text = "Прибуток становитиме ${round(profitValue)[0]} грн"
+        }
 
+        fun calculateP(PcValue: Double, sigmaValue: Double, BValue: Double): Double {
+            val profitValue =  0.0
+            return profitValue
+        }
+
+        doBtn.setOnClickListener {
+            val PcValue = Pc_input.text.toString().toDoubleOrNull()
+            val sigmaValue= sigma_input.text.toString().toDoubleOrNull()
+            val BValue= B_input.text.toString().toDoubleOrNull()
+
+            if (PcValue == null) {
+                error_text1.text = "Середньодобова потужність повинна містити число"
+                return@setOnClickListener
+            }
+            if (sigmaValue == null) {
+                error_text2.text = "Середньоквадратичне відхилення повинно містити число"
+                return@setOnClickListener
+            }
+            if (BValue == null) {
+                error_text3.text = "Вартість повинна містити число"
+                return@setOnClickListener
+            }
+
+            error_text1.text = ' '.toString()
+            error_text2.text = ' '.toString()
+            error_text3.text = ' '.toString()
+
+            val profitValue = calculateP(PcValue, sigmaValue, BValue)
+
+            showResult(profitValue)
+        }
+
+        resetBtn.setOnClickListener{
+            error_text1.text = ' '.toString()
+            error_text2.text = ' '.toString()
+            error_text3.text = ' '.toString()
+            Pc_input.text.clear()
+            sigma_input.text.clear()
+            B_input.text.clear()
+        }
     }
 }
