e73fdffdd86f39c150bffda3b31b5857f0c77874
Victoriia-Ripka <vikaripka@gmail.com>
2024-11-12 11:52:04 +0100

    lab5 componets decomposition

--- PW-5_TV-13_NowotkaViktoriia/app/src/main/java/com/example/pw_5/ui/calculators/Calculator1Screen.kt
+++ PW-5_TV-13_NowotkaViktoriia/app/src/main/java/com/example/pw_5/ui/calculators/Calculator1Screen.kt
@@ -1,10 +1,46 @@
 package com.example.pw_5.ui.calculators
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.pw_5.ui.components.FancyButton
+import com.example.pw_5.ui.components.Header
+import com.example.pw_5.ui.components.Title
 
 @Composable
 fun Calculator1Screen(
     goBack: () -> Unit
 ) {
-    
+    Column(){
+        Header()
+
+        Title(
+            text="Завдання 1" ,
+            textStyle = TextStyle(fontSize = 14.sp)
+        )
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+
+
+        FancyButton(
+            text="Розрахувати",
+            onClick = {}
+        )
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        FancyButton(
+            text="Повернутися назад",
+            onClick = {}
+        )
+    }
+
+
 }
---
+++ PW-5_TV-13_NowotkaViktoriia/app/src/main/java/com/example/pw_5/ui/components/header.kt
@@ -0,0 +1,36 @@
+package com.example.pw_5.ui.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.pw_5.ui.entry.titleBoxModifier
+
+
+@Composable
+fun Header(){
+    Column(
+        modifier = titleBoxModifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly
+    ){
+        Text(
+            text = "Практична робота №5.",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.SemiBold
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        Text(
+            text = "Розробник: Новотка Вікторія",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.SemiBold)
+    }
+
+}
